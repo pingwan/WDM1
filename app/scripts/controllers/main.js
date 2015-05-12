8bd4445eed32d11388a5a892356e87924a578f9b
Ka-Ping Wan <k.p.wan92@gmail.com>
2015-05-12 20:13:44 +0200

    added table templte

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,26 @@
  */
 angular.module('wdm1App')
   .controller('MainCtrl', ['$scope', '$http', 'x2js', '$log', 'queryFactory', function ($scope, $http, x2js, $log, queryFactory) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+  var movieOne = {
+    title: "Harry Potter",
+    year: '2008',
+    country : 'USA',
+    genre : 'Drama',
+    director: 'Steven Spielberg',
+    actors: ['Christian den boer','Ping Wan','Roeland Oosterloo']
+  }
+
+  var movieTwo = {
+    title: "Lost",
+    year: '2012',
+    country : 'Japan',
+    genre : 'Love',
+    director: 'Abba',
+    actors: ['Christian den boer','Ping Wan','Roeland Oosterloo']
+  }
+
+  $scope.movies  = [movieOne,movieTwo];
 
     queryFactory.executeMovieQuery('//movies/movie/title', function(data) {
         $log.log(data);
