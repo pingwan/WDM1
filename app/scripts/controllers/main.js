bdc1dda19c3c91cca619cae76de4171b0834dce1
Roeland Oosterloo <roeland.1992@gmail.com>
2015-05-23 22:55:32 +0200

    working i guess

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,90 +8,44 @@
  * Controller of the wdm1App
  */
 angular.module('wdm1App')
-    .controller('MainCtrl', ['$scope', '$http', 'x2js', '$log', 'queryFactory', '$location', function ($scope, $http, x2js, $log, queryFactory, $location) {
-        $scope.movies = [];
-        //fill initial list
-        queryFactory.executeMovieQuery('//movies/movie', function(data) {
-            setResult(data);
-        });
+    .controller('MainCtrl', ['$scope', '$http', 'x2js', '$log',  function ($scope, $http, x2js, $log) {
+        $scope.songs = [];
 
-        $scope.applyFilter = function(event){
-            $scope.movies = [];
-            if($scope.titleKeywords && $scope.titleKeywords != '') {
-                queryFactory.executeMovieQuery('//movies/movie[title[contains(text(),\'' + $scope.titleKeywords  +'\')]]', function(data) {
-                    setResult(data);
-                });
+        $http.get("http://localhost?method=all", {
+          transformResponse: function (data) {
+            var json = x2js.xml_str2json(data);
+            return json.result;
+          }
+        }).success(function (data) {
+            $scope.songs = data.songs.score;
+          });
+        for( var i = 0 ; i < $scope.songs.length; i++ ) {
+          var instuments = $scope.songs[i].instuments.instrument;
+          $scope.songs[i].instuments = [];
+          if(instuments && angular.isArray(instuments)) {
+            for (var j = 0; j < instuments.length; j++) {
+              $scope.songs[i].instuments.push({name: instuments[j]});
             }
-
-            if($scope.yearOption === 'before' && $scope.yearValue && $scope.yearValue != ''){
-                queryFactory.executeMovieQuery('//movies/movie[year <' + $scope.yearValue  +']', function(data) {
-                    setResult(data);
-                });
+          }
+          else if (instuments && !angular.isArray(instuments)) {
+            $scope.songs[i].instuments.push({name:instuments})
+          }
+        }
+        $scope.applyFilter = function() {
+          var query = "?method=filter"
+          if($scope.titleKeywords) {
+            query += "&title=" + $scope.titleKeywords;
+          }
+          if($scope.componistKeywords) {
+            query += "&componist=" + $scope.componistKeywords;
+          }
+          $http.get("http://localhost" + query, {
+            transformResponse: function (data) {
+              var json = x2js.xml_str2json(data);
+              return json.result;
             }
-
-            if($scope.yearOption === 'after' && $scope.yearValue && $scope.yearValue != ''){
-                queryFactory.executeMovieQuery('//movies/movie[year >' + $scope.yearValue  +']', function(data) {
-                    setResult(data);
-                });
-            }
-
-            if($scope.genre !== undefined){
-                queryFactory.executeMovieQuery('//movies/movie[genre =\"' + $scope.genre  +'\"]', function(data) {
-                    setResult(data);
-                });
-            }
-
-            if($scope.summaryKeywords && $scope.summaryKeywords != ''){
-                queryFactory.executeMovieQuery('//movies/movie[summary[contains(text(),\'' + $scope.summaryKeywords  +'\')]]', function(data) {
-                    setResult(data);
-                });
-            }
+          }).success(function (data) {
+            $scope.songs = data.songs.score;
+          });
         };
-
-        $scope.gotoDetail = function(movie) {
-            $log.log('#/details/' + movie.title);
-            $location.path('details/' + movie.title);
-        }
-
-        /*
-         * deze function moet je hebben voor het geval deze resultaat maar uit 1 object bestaat,
-         * dan moet je hem ff in een array gooien ;)
-         */
-        var setResult = function(data){
-            if($scope.movies.length == 0) {
-                if(!angular.isArray(data.movie)) {
-                    $scope.movies = [data.movie];
-                }
-                else {
-                    $scope.movies = data.movie;
-                }
-            }
-            else {
-                if (!angular.isArray(data.movie)) {
-                    var arr = [[data.movie], $scope.movies];
-                }
-                else {
-                    var arr = [data.movie, $scope.movies];
-                }
-
-                $log.log(arr);
-                var res = [];
-                for(var i = 0; i < arr[0].length; i++) {
-                    for(var j = 0; j < arr[1].length; j++) {
-                        if(arr[0][i].title == arr[1][j].title) {
-                            res.push(arr[0][i]);
-                        }
-                    }
-                }
-
-                $scope.movies = res;
-            }
-        }
-
-    }])
-    .controller('DetailCtrl', ['$scope', '$http', 'x2js', '$log', 'queryFactory', '$routeParams', function ($scope, $http, x2js, $log, queryFactory, $routeParams) {
-        queryFactory.executeMovieQuery('//movies/movie[title =\"' + $routeParams.title  +'\"]', function(data) {
-            $log.log(data.movie);
-            $scope.movie = data.movie;
-        });
     }]);
